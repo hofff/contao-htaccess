3b54ff1cbaf687c190f63b0ea451a4ee9aa9a988
Tristan Lins <tristan.lins@infinitysoft.de>
2012-01-20 13:01:49 +0100

    Remove default empty htaccess.php configuration file, to prevent overwrite on update.

--- system/modules/htaccess/HtaccessConfig.php
+++ system/modules/htaccess/HtaccessConfig.php
@@ -122,9 +122,52 @@ class HtaccessConfig extends Config
 	 */
 	protected function initialize()
 	{
+		$strFile = TL_ROOT . '/system/config/htaccess.php';
+
+		/**
+		 * On new installations, the config file does not exists!
+		 */
+		if (!is_file($strFile)) {
+			$this->strHtaccessTop = '<?php if (!defined(\'TL_ROOT\')) die(\'You can not access this file directly!\');
+
+/**
+ * htaccess Generator
+ * Copyright (C) 2011 Tristan Lins
+ *
+ * Extension for:
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  InfinitySoft 2011
+ * @author     Tristan Lins <tristan.lins@infinitysoft.de>
+ * @package    htaccess Generator
+ * @license    LGPL
+ * @filesource
+ */';
+			$this->strHtaccessBottom = '?>';
+			return;
+		}
+
 		// Read the htaccess configuration file
 		$strMode = 'top';
-		$resFile = fopen(TL_ROOT . '/system/config/htaccess.php', 'rb');
+		$resFile = fopen($strFile, 'rb');
 
 		while (!feof($resFile))
 		{
